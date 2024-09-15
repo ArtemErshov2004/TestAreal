b8324ef63f1a1d76974bfaecf18f705bdaaf7c62
ArtemErshov2004 <mr.artem.ershov.2004@mail.ru>
2024-09-16 01:02:32 +0300

    Настройка маршрутизации

--- FrontEnd/blog/src/router/index.js
+++ FrontEnd/blog/src/router/index.js
@@ -1,27 +1,20 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import ArticlesList from '../components/ArticlesList.vue';
+import ArticleView from '../components/ArticleView.vue';
+import FilteredComments from '../components/FilteredComments.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    { path: '/', component: ArticlesList },
+    { path: '/articles/:id', component: ArticleView, props: true },
+    { path: '/filtered-comments', component: FilteredComments }
+];
 
 const router = new VueRouter({
-  routes
-})
+    mode: 'history',
+    routes
+});
 
-export default router
+export default router;
